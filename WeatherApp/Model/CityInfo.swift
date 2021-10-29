aa92dca6d08597b964fd67fefb178afe70ae2699
Xuanwei Zhang <christinezxw@hotmail.com>
2021-10-29 02:45:32 -0700

    add code

---
+++ WeatherApp/Model/CityInfo.swift
@@ -0,0 +1,22 @@
+//
+//  CityInfo.swift
+//  WeatherApp
+//
+//  Created by Xuanwei Zhang on 10/28/21.
+//
+
+import Foundation
+import RealmSwift
+
+class CityInfo : Object {
+    @objc dynamic var key: String  = ""
+    @objc dynamic var type : String = ""
+    @objc dynamic var localizedName : String = ""
+    @objc dynamic var countryLocalizedName : String = ""
+    @objc dynamic var administrativeID : String = ""
+    
+    override static func primaryKey() -> String? {
+        return "key"
+    }
+    
+}
---
+++ WeatherApp/Model/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Xuanwei Zhang on 10/28/21.
+//
+
+import Foundation
+
+class CurrentWeather{
+    var cityKey : String = ""
+    var cityInfoName : String = ""
+    var weatherText : String = ""
+    var epochTime : Int = Int.min
+    var isDayTime : Bool = true
+    var temp : Int = Int.min
+}
